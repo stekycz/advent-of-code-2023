d18a89c833712c7434334c8bd558ce976553a09a
Martin Štekl <martin.stekl@gmail.com>
2023-12-04 02:56:50 +0100

    Add 02 1st start solution

---
+++ src/02/star1.ts
@@ -0,0 +1,100 @@
+import * as A from 'fp-ts/Array';
+import * as O from 'fp-ts/Option';
+import { pipe } from 'fp-ts/function';
+import { execute } from '../executor';
+
+interface Game<T> {
+  id: T;
+  parts: T;
+}
+
+interface GamePart {
+  red: number;
+  green: number;
+  blue: number;
+}
+
+interface ColorCountGroups {
+  color: string;
+  count: string;
+}
+
+function isColorCountGroups(value: unknown): value is ColorCountGroups {
+  return (
+    typeof value === 'object' &&
+    value !== null &&
+    'color' in value &&
+    typeof value.color === 'string' &&
+    'count' in value &&
+    typeof value.count === 'string'
+  );
+}
+
+const GAME_LINE_REGEX = /^Game (?<id>\d+): (?<games>.*)$/;
+
+const getGamePart = (game: string): GamePart =>
+  pipe(
+    game.split(','),
+    A.map((part) => /(?<count>\d+) (?<color>red|green|blue)/.exec(part)),
+    A.map(O.fromNullable),
+    A.filter(O.isSome),
+    A.map((part) => O.fromNullable(part.value.groups)),
+    A.filter(O.isSome),
+    A.map((groups) => groups.value),
+    A.filter(isColorCountGroups),
+    A.map((groups) => ({ [groups.color]: Number.parseInt(groups.count, 10) })),
+    A.reduce({ red: 0, green: 0, blue: 0 }, (acc: GamePart, game) => ({
+      ...acc,
+      ...game,
+    })),
+  );
+
+const getGames = (games: string[]): GamePart[] =>
+  pipe(
+    games,
+    A.flatMap((game) => game.split(';')),
+    A.map(getGamePart),
+  );
+
+const isGameValid = (
+  game: Game<O.Option<string>>,
+): game is Game<O.Some<string>> => O.isSome(game.id) && O.isSome(game.parts);
+
+const max: GamePart = {
+  red: 12,
+  green: 13,
+  blue: 14,
+};
+
+const getResult = (lines: string[]): number =>
+  pipe(
+    lines,
+    A.map((line) => O.fromNullable(GAME_LINE_REGEX.exec(line))),
+    A.filter(O.isSome),
+    A.map((match) => O.fromNullable(match.value.groups)),
+    A.filter(O.isSome),
+    A.map((groups) => ({
+      id: O.fromNullable(groups.value['id']),
+      parts: O.fromNullable(groups.value['games']),
+    })),
+    A.filter(isGameValid),
+    A.map((match) => ({
+      id: Number.parseInt(match.id.value, 10),
+      parts: getGames(match.parts.value.split(',')),
+    })),
+    A.filter((game) => {
+      return pipe(
+        game.parts,
+        A.every(
+          (game) =>
+            game.red <= max.red &&
+            game.green <= max.green &&
+            game.blue <= max.blue,
+        ),
+      );
+    }),
+    A.map((game) => game.id),
+    A.reduce(0, (acc, id) => acc + id),
+  );
+
+execute(getResult);
